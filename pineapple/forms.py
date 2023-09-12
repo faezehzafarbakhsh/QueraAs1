15345a927b1f82b0843a2763cd96282bf67108cd
darwvin <darwvin@hotmail.com>
2023-09-12 10:56:53 +0330

    done

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,4 +1,4 @@
-
+from django import forms
 
 class SellerForm:
     pass
@@ -7,7 +7,13 @@ class PineappleForm:
     pass
 
 class OrderForm:
-    pass
+    pineapple_weight = forms.IntegerField(label='وزن آناناس (کیلوگرم)')
+    def clean_pineapple_weight(self):
+        weight = self.cleaned_data['pineapple_weight']
+        if weight > 100:
+            raise forms.ValidationError('شما تنها می‌توانید حداکثر ۱۰۰ کیلوگرم آناناس سفارش دهید.')
+        return weight
+
 
 class SubscriptionForm:
     pass
--- pineapple/urls.py
+++ pineapple/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 app_name = "pineapple"
 
 urlpatterns = [
-    
+    path('order-list', views.order_list_view),
+    path('order-detail', views.order_detail_view),
+    path('order-create', views.order_create_view),
+    path('order-update', views.order_update_view)
 ]
 
 if settings.DEBUG:
