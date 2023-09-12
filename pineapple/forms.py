6f7f14d082640d31640a57362cb9c322b0b6b7aa
arman <cheraghvandiarman@gamil.com>
2023-09-12 21:31:57 +0430

    b

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,5 +1,6 @@
-
-
+from django import forms
+from .models import Subscription
+import re
 class SellerForm:
     pass
 
@@ -9,8 +10,12 @@ class PineappleForm:
 class OrderForm:
     pass
 
-class SubscriptionForm:
-    pass
+# class SubscriptionForm:
+    
+#         phone_regex = r'^09\d{9}$'
+#         if not re.match(phone_regex, phone_number):
+#             raise forms.ValidationError('شماره تلفن اشتباه است. شماره تلفن باید ۱۱ رقم باشد و با ۰۹ شروع شود.')
+#         return phone_number
 
 class CommentForm:
     pass
