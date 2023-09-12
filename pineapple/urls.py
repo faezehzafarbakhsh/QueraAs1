93cf92f507ef3fa4f7d0a2b851138105c314342f
darwvin <darwvin@hotmail.com>
2023-09-12 20:34:37 +0330

    done

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 app_name = "pineapple"
 
 urlpatterns = [
-    path('order-list', views.order_list_view),
-    path('order-detail', views.order_detail_view),
-    path('order-create', views.order_create_view),
-    path('order-update', views.order_update_view)
+    path('order-list', views.order_list_view, name='order-list'),
+    path('order-detail', views.order_detail_view, name='order-detail'),
+    path('order-create', views.order_create_view, name='order-create'),
+    path('order-update', views.order_update_view, name='order-update'),
+    path('comment-create', views.comment_create_view, name='comment-create'),
 ]
 
 if settings.DEBUG:
