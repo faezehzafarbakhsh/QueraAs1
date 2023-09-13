83116063e67a5b277c19939362c85c9739101686
Afshin <khodeafshin@gmail.com>
2023-09-13 20:09:16 +0330

    url/model

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,7 +1,24 @@
 
 
-class SellerForm:
-    pass
+class SellerForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ['name' , 'address', 'certificate_code']
+        
+    def clean_address(self):
+        address = self.cleaned_data.get('address')
+        
+        if len(address) < 10:
+            raise ValidationError('این فیلد باید بیشتر از ۱۰ کاراکتر باشد.')
+        return address
+    
+    def clean_certificate_code(self):
+        certificate_code = self.cleaned_data.get('certificate_code')
+        
+        if certificate_code.isupper() == False:
+            raise ValidationError('حروف گواهینامه باید حروف بزرگ باشد.')
+        return certificate_code
+    
 
 class PineappleForm:
     pass
